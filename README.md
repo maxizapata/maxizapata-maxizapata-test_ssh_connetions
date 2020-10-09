f518d5e5176d15b99bcb8144648b0132a3abeb34
Maximiliano Zapata <maxi@MacBook-Air-de-Maximiliano.local>
2020-10-09 14:30:04 -0300

    README added

---
+++ README.md
@@ -0,0 +1,14 @@
+# Chech ssh connections
+Simple script to verify your ssh conection work.
+
+## Usage
+To use this script you must install paramiko with this command:
+
+```bash
+pip install paramiko
+```
+Only works with python 3.6 or later
+
+## Autor
+Maximiliano Zapata
+maxzm7@gmail.com
--- check_ssh_list.py
+++ check_ssh_list.py
@@ -1,4 +1,11 @@
+# This script allow to test a list of ssh conections
+# You must pass two arguments:
+# 1 - The file that contains the list of host
+# 2 - The user to connect in the remote host
+# 3 (Optinal) - The private ssh key
+
 from sys import argv, stderr
+from os import environ as env
 try:
     import paramiko
 except ModuleNotFoundError:
@@ -6,10 +13,22 @@ except ModuleNotFoundError:
 else:
     ssh = paramiko.SSHClient()
 
-failed_connections = []
+
+def private_key():
+    try:
+        argv[3]
+    except IndexError:
+        priv_key = f"{env['HOME']}/.ssh/id_rsa"
+    else:
+        priv_key = argv[3]
+    return priv_key
+
+hosts_not_reached = []
 success_connections = []
+error_connections = []
 data_source = argv[1]
 username = argv[2]
+priv_key = private_key()
 
 stderr = None
 OKBLUE = '\033[94m'
@@ -17,13 +36,23 @@ FAIL = '\033[91m'
 ENDC = '\033[0m'
 
 with open(argv[1]) as ssh_list:
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     for ip_raw in ssh_list:
         ip = ip_raw.strip()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        print(f'Testing connection for {ip}')
         try:
-            ssh.connect(ip, username=username, key_filename=argv[3])
-        except paramiko.ssh_exception.SSHException:
-            failed_connections.append(ip)
+            # Try if can connect to remote server
+            ssh.connect(ip, username=username, key_filename=priv_key)
+        except (paramiko.ssh_exception.SSHException, TimeoutError):
+            # Cannot reach
+            print(FAIL + f'Host {ip} cannot be reached' + ENDC)
+            hosts_not_reached.append(ip)
+        except ValueError:
+            # Connot login with the current ssh key
+            # Trying to connect via password
+            # Pending to implement
+            error_connections.append(ip)
         else:
             success_connections.append(ip)
 
@@ -32,9 +61,7 @@ def print_connections(server_list, msg, font_color):
         print(font_color + f'{msg}:' + ENDC)
         for f in server_list:
             print(f'- {f}')
-    else:
-        print(font_color + f'No {msg.lower()}' + ENDC)
 
-print_connections(failed_connections, 'Failed Connections', FAIL)
-
+print_connections(hosts_not_reached, 'Host not reached', FAIL)
+print_connections(error_connections, 'Error connections', FAIL)
 print_connections(success_connections, 'Success connections', OKBLUE)
